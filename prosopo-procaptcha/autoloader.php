bae24465e58262ca69eef1ec41ff777e56b8c876
Maxim Akimov <lightsourcee@pm.me>
2024-12-23 10:14:08 +0200

    github actions

---
+++ prosopo-procaptcha/autoloader.php
@@ -0,0 +1,7 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+// the scoper-autoloader includes autoloader for this namespace as well.
+require_once __DIR__ . '/prefixed-vendors/vendor/scoper-autoload.php';
+require_once __DIR__ . '/src/helpers.php';
--- prosopo-procaptcha/prosopo-procaptcha.php
+++ prosopo-procaptcha/prosopo-procaptcha.php
@@ -15,8 +15,6 @@ namespace Io\Prosopo\Procaptcha;
 
 defined( 'ABSPATH' ) || exit;
 
-// the scoper-autoloader includes autoloader for this namespace as well.
-require_once __DIR__ . '/prefixed-vendors/vendor/scoper-autoload.php';
-require_once __DIR__ . '/src/helpers.php';
+require_once __DIR__ . '/autoloader.php';
 
 ( new Plugin( __FILE__ ) )->set_hooks( is_admin() );

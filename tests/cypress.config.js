18617301ab633b0ae81769ad62dd938fc73403d4
Maxim Akimov <lightsourcee@pm.me>
2025-01-20 16:07:20 +0200

    tests

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,20 +1,40 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
+const fs = require('fs');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      require("cypress-terminal-report/src/installLogsPrinter")(on, {
-        outputRoot: config.projectRoot + "/cypress/",
-        specRoot: "cypress/e2e",
-        outputTarget: {
-          "logs|html": "html",
+    video: true,
+    e2e: {
+        setupNodeEvents(on, config) {
+            require("cypress-terminal-report/src/installLogsPrinter")(on, {
+                outputRoot: config.projectRoot + "/cypress/",
+                specRoot: "cypress/e2e",
+                outputTarget: {
+                    "logs|html": "html",
+                },
+            });
+            on(
+                'after:spec',
+                (spec, results) => {
+                    if (!results || !results.video) {
+                        return;
+                    }
+
+                    const failures = results.tests.some((test) =>
+                        test.attempts.some((attempt) => attempt.state === 'failed')
+                    );
+
+                    if (failures) {
+                        return;
+                    }
+
+                    // fixme fs.unlinkSync(results.video);
+                }
+            )
         },
-      });
+        baseUrl: "http://procaptcha.local",
+        watchForFileChanges: false, // Disable auto-run on file changes.
+        defaultCommandTimeout: 6000, // default 4000.
+        viewportWidth: 1280, // default 1000.
+        viewportHeight: 720, // default 660.
     },
-    baseUrl: "http://procaptcha.local",
-    watchForFileChanges: false, // Disable auto-run on file changes.
-    defaultCommandTimeout: 6000, // default 4000.
-    viewportWidth: 1280, // default 1000.
-    viewportHeight: 720, // default 660.
-  },
 });

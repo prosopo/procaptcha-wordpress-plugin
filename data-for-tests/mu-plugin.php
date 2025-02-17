005f13596bf98c95aa9dcf3a90db82fb3b11469b
Maxim Akimov <lightsourcee@pm.me>
2025-02-17 16:06:58 +0200

    Jetpack integration tests

--- data-for-tests/mu-plugin.php
+++ data-for-tests/mu-plugin.php
@@ -6,7 +6,6 @@ add_filter( 'comment_flood_filter', '__return_false', 999 );
 add_filter( 'pre_wp_mail', '__return_true' );
 add_filter( 'bbp_bypass_check_for_flood', '__return_true' );
 add_filter( 'wpforms_process_time_limit_check_bypass', '__return_true' );
-add_filter( 'jetpack_gutenberg', '__return_true' );
 add_filter(
 	'swpm_get_current_page_url_filter',
 	function ( string $url ): string {

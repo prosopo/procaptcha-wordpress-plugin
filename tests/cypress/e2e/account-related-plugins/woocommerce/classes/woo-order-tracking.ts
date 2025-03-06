0f8763b40339d66986a717d3786bbd39d298ecfe
Maxim Akimov <lightsourcee@pm.me>
2025-03-06 16:20:46 +0200

    v 1.12.0

--- tests/cypress/e2e/account-related-plugins/woocommerce/classes/woo-order-tracking.ts
+++ tests/cypress/e2e/account-related-plugins/woocommerce/classes/woo-order-tracking.ts
@@ -16,7 +16,7 @@ class WooOrderTracking extends FormTest {
 			captchaInput: "",
 		};
 		this.submitValues = {
-			orderid: "692",
+			orderid: "1131",
 			order_email: "test@gmail.com",
 		};
 		this.messages = {
